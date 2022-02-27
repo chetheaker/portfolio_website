0255ca94f54c042c7b891a730eff252688338226
chetheaker <che.theaker@gmail.com>
2022-02-27 18:58:49 +0100

    popups for 4 projects completed

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 
-// change name from projectOne to the project name
+// change name to the project name
+// function to open popup for project1
 function projectOnePopUp() {
-    let popUp = document.getElementById("project1");
-    popUp.style.display = "block";
+    document.getElementById("project1").style.display = "block";
   }
+// function to close popup for project1
+function projectOneHide() {
+    document.getElementById("project1").style.display = "none";
+}
+
+// change name to the project name
+// function to open popup for project2
+function projectTwoPopUp() {
+    document.getElementById("project2").style.display = "block";
+  }
+// function to close popup for project2
+function projectTwoHide() {
+    document.getElementById("project2").style.display = "none";
+}
+
+// change name to the project name
+// function to open popup for project3
+function projectThreePopUp() {
+    document.getElementById("project3").style.display = "block";
+  }
+// function to close popup for project3
+function projectThreeHide() {
+    document.getElementById("project3").style.display = "none";
+}
+
+// change name to the project name
+// function to open popup for project4
+function projectFourPopUp() {
+    document.getElementById("project4").style.display = "block";
+  }
+// function to close popup for project4
+function projectFourHide() {
+    document.getElementById("project4").style.display = "none";
+}
